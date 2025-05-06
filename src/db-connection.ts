c5a6aebe5596b8f207d78321e48c92cb6e5872a5
Adrian <eslenderdark@gmail.com>
2025-05-06 15:56:46 +0200

    db-conection updat to render

--- src/db-connection.ts
+++ src/db-connection.ts
@@ -1,9 +1,17 @@
 import { Pool } from 'pg';
 
+// const pool = new Pool({
+//   user: 'postgres',
+//   password: '1234',
+//   host: 'localhost',
+//   port: 5432, // default Postgres port
+//   database: 'clashofclans'
+// });
+
 const pool = new Pool({
-  user: 'postgres',
-  password: '1234',
-  host: 'localhost',
+  user: 'clashofclans_user',
+  password: 'kkklJzDEp8tLAz549v2B5SjBSADqwaWM',
+  host: 'dpg-d08eufvgi27c738hev00-a',
   port: 5432, // default Postgres port
   database: 'clashofclans'
 });
